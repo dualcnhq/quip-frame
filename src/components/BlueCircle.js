28c1495085b4884f8e95cbf33974e7029dcb12d1
Clau Yagyagan <dualcnhq@gmail.com>
2018-08-07 11:17:36 +0800

    Update code structure, update blue circle view

---
+++ src/components/BlueCircle.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class BlueCircle extends Component {
+  render() {
+    return (
+      <div className='blue-circle'></div>
+    );
+  }
+}
+
+export default BlueCircle;
